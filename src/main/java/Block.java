089abee32924e1d3eb5deea942aec1df47106d32
Marwan02 <marwan.lachelah2@gmail.com>
2021-05-08 13:50:50 +0200

    First Commit

---
+++ src/main/java/Block.java
@@ -0,0 +1,31 @@
+import Utils.StringUtil;
+
+import java.util.Date;
+
+public class Block {
+
+    public String hash;
+    public String previousHash;
+    private String data;
+    private long timeStamp;
+
+    public Block(String data,String previousHash ) {
+        this.data = data;
+        this.previousHash = previousHash;
+        this.timeStamp = new Date().getTime();
+        this.hash = calculateHash();
+        System.out.println("## New Block Added("+hash+") : "+data);
+    }
+
+    public String calculateHash() {
+        String calculatedhash = StringUtil.applySha256(
+                previousHash +
+                        Long.toString(timeStamp) +
+                        data
+        );
+        return calculatedhash;
+    }
+
+
+
+}
