6bc8e147e8ab57704e8407f5815a5ed18d70c913
Marwan02 <marwan.lachelah2@gmail.com>
2021-05-08 14:06:46 +0200

    cleaning

--- src/main/java/Block.java
+++ src/main/java/Block.java
@@ -9,23 +9,21 @@ public class Block {
     private String data;
     private long timeStamp;
 
-    public Block(String data,String previousHash ) {
+    public Block(String data, String previousHash) {
         this.data = data;
         this.previousHash = previousHash;
         this.timeStamp = new Date().getTime();
         this.hash = calculateHash();
-        System.out.println("## New Block Added("+hash+") : "+data);
+        System.out.println("## New Block Added(" + hash + ") : " + data);
     }
 
     public String calculateHash() {
-        String calculatedhash = StringUtil.applySha256(
+        return StringUtil.applySha256(
                 previousHash +
-                        Long.toString(timeStamp) +
+                        timeStamp +
                         data
         );
-        return calculatedhash;
     }
-
 
 
 }
